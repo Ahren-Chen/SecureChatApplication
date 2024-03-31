162c1bbf2093576eba02a0f3f779337f0f4ed62f
Ahren Chen <ahren@hotmail.ca>
2024-03-30 21:57:18 -0400

    Renaming Updates

--- app/src/main/java/com/example/securechatapplication/MessagesFragment.java
+++ app/src/main/java/com/example/securechatapplication/MessagesFragment.java
@@ -9,11 +9,11 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-import com.example.securechatapplication.databinding.FragmentSecondBinding;
+import com.example.securechatapplication.databinding.FragmentMessagesBinding;
 
-public class SecondFragment extends Fragment {
+public class MessagesFragment extends Fragment {
 
-    private FragmentSecondBinding binding;
+    private FragmentMessagesBinding binding;
 
     @Override
     public View onCreateView(
@@ -21,7 +21,7 @@ public class SecondFragment extends Fragment {
             Bundle savedInstanceState
     ) {
 
-        binding = FragmentSecondBinding.inflate(inflater, container, false);
+        binding = FragmentMessagesBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
     }
@@ -32,8 +32,8 @@ public class SecondFragment extends Fragment {
         binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_HomeFragment);
+                NavHostFragment.findNavController(MessagesFragment.this)
+                        .navigate(R.id.action_MessagesFragment_to_HomeFragment);
             }
         });
     }
