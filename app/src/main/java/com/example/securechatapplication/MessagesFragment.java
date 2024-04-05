a573604f6d56dc9e9836450a50f7679cf931a82a
Jiaming Li <ljm-jim@hotmail.com>
2024-04-05 16:18:12 -0400

    MessageInitial

--- app/src/main/java/com/example/securechatapplication/MessagesFragment.java
+++ app/src/main/java/com/example/securechatapplication/MessagesFragment.java
@@ -7,34 +7,42 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
-
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import com.example.securechatapplication.databinding.FragmentMessagesBinding;
 
 public class MessagesFragment extends Fragment {
-
+    private EditText editTextMessage;
+    private Button buttonSend;
+    private TextView textViewChat;
     private FragmentMessagesBinding binding;
 
     @Override
-    public View onCreateView(
-            @NonNull LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_messages, container, false);
 
-        //Create the view
-        binding = FragmentMessagesBinding.inflate(inflater, container, false);
-        return binding.getRoot();
+        editTextMessage = view.findViewById(R.id.editTextMessage);
+        textViewChat = view.findViewById(R.id.textViewChat);
+        buttonSend = view.findViewById(R.id.buttonSend);
 
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage();
+            }
+        });
+
+        return view;
     }
 
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        //If the home "Previous" is clicked
-        binding.buttonHome.setOnClickListener(view1 -> NavHostFragment.findNavController(MessagesFragment.this)
-                .navigate(R.id.action_MessagesFragment_to_HomeFragment));
+    private void sendMessage() {
+        String message = editTextMessage.getText().toString().trim();
+        if (!message.isEmpty()) {
+            textViewChat.append(message + "\n");
+            editTextMessage.setText("");
+        }
     }
-
     @Override
     public void onDestroyView() {
         super.onDestroyView();
